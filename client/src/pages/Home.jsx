e6c983c6d53fe7a58528dc1bdb1bf238f74d06c7
keonshu@gmail.com <keonplaysguitar@gmail.com>
2024-08-11 02:56:47 +0800

    fixed carousel UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../carousel.css'; // Ensure this path matches your CSS file location
 import carousell1 from '../assets/carousell1.png';
@@ -16,30 +16,30 @@ function Home() {
 
   return (
     <div>
-      <div className="static-overlay">
-        <h1 className="header-text">JOIN OUR<br />COMMUNITY</h1>
-        <Link to='/register'>
-          <button className="cta-button">Join Now</button>
-        </Link>
-      </div>
       <div className="carousel-container">
         <Carousel
           showThumbs={false}
           autoPlay
           infiniteLoop
           centerMode
-          centerSlidePercentage={40} // Adjust this to control the visible part of the adjacent slides
+          centerSlidePercentage={60} // Adjust this to make the center slide more prominent
           swipeable={true}
           selectedItem={currentIndex}
           onChange={handleChange}
         >
-          <div className={`carousel-slide ${currentIndex === 0 ? 'center-slide' : ''}`}>
+          <div className="carousel-slide">
             <img src={carousell1} alt="Slide 1" />
           </div>
-          <div className={`carousel-slide ${currentIndex === 1 ? 'center-slide' : ''}`}>
+          <div className="carousel-slide">
             <img src={carousel2} alt="Slide 2" />
           </div>
-          <div className={`carousel-slide ${currentIndex === 2 ? 'center-slide' : ''}`}>
+          <div className="carousel-slide center-slide">
+            <div className="static-overlay">
+              <h1 className="header-text">JOIN OUR<br />COMMUNITY</h1>
+              <Link to='/register'>
+                <button className="cta-button">Join Now</button>
+              </Link>
+            </div>
             <img src={carousel3} alt="Slide 3" />
           </div>
         </Carousel>
