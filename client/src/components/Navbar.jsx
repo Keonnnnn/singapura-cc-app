5409163db984a698d3a535edc2baab7eb22c2472
keonshu@gmail.com <keonplaysguitar@gmail.com>
2024-08-11 21:57:19 +0800

    removed notifications false link from navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -287,9 +287,6 @@ const Navbar = () => {
                   <Typography>Facilities</Typography>
                 </Link>
 
-                <Link to="/notifications">
-                  <Typography>Notification</Typography>
-                </Link>
 
                 {user && user.role === "Customer" && (
                   <Link to="/posts">
